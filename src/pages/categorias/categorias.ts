5642e583209fd6a4984cb7ca8df748ea726c35eb
Guilherme Mascarenhas Barbosa <gmbarbosa@stefanini.com>
2018-12-18 11:29:33 -0200

    Listando categorias do Backend-Heroku

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { CategoriaService } from '../../services/domain/categoria.service';
+import { CategoriaDTO } from '../../models/categoria.dto';
+import { API_CONFIG } from '../../config/api.config';
 
 /**
  * Generated class for the CategoriasPage page.
@@ -15,10 +18,36 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CategoriasPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  items: CategoriaDTO[];
+
+  bucketUrl: string = API_CONFIG.bucketBaseUrl;
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public categoriaService: CategoriaService) {
+
   }
 
+  /*
   ionViewDidLoad() {
-    console.log('ionViewDidLoad CategoriasPage');
+    this.categoriaService.findAll()
+    .subscribe(response => {
+      console.log(response);
+    }, error => {
+      console.log(error);
+    });
+  }*/
+
+  ionViewDidLoad() {
+    this.categoriaService.findAll()
+      .subscribe(response => {
+        this.items = response;
+      },
+      error => {});
+  }
+
+  showProdutos(categoria_id : string) {
+    this.navCtrl.push('ProdutosPage', {categoria_id: categoria_id});    
   }
 }
